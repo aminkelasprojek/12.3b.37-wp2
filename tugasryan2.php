97bf8791dffed2eb04fae8beba022f2e1e31ab93
Ryan Dwi Syawaludin <101648113+ryands1102@users.noreply.github.com>
2022-03-29 01:46:03 +0700

    Add files via upload

---
+++ tugasryan2.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class tugasryan2 extends CI_Controller {
+
+    public function index()
+    {
+        echo "Nama : Ryan Dwi Syawaludin";
+        echo "Nim : 12207169";
+        echo "Kelas : 12.3B.37";
+    }
+
+}
+
+
+/*"End of file tugasryan2.php */
